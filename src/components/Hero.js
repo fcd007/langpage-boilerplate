d0ee2c6ce9bd6963ede4d1caea01654849bfb4ba
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-02 08:54:30 -0300

    docs: add storybooks documents

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => (
+  <div>
+    <div>
+      <ul>
+        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+        <li> Suscipit sed officia quae aut, rerum tempore adipisci.</li>
+        <li> eaque voluptates quas, officiis aspernatur veniam ut.</li>
+        <li>dolores molestias! Maxime deleniti iste similique molestiae!</li>
+      </ul>
+    </div>
+    <button type="submit">Matricule-se Já</button>
+  </div>
+);
+
+export default Hero;
