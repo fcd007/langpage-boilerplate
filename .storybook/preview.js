9fdef9b834494b3c0582f1aac50a582c82b88649
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-03 19:37:28 -0300

    feat: ensure add viewports storybooks config media

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { addDecorator } from "@storybook/react";
+import { addDecorator, addParameters } from "@storybook/react";
 import GlobalStyle from "../src/components/Global-style";
 
 addDecorator((storyFn) => (
@@ -8,3 +8,47 @@ addDecorator((storyFn) => (
     {storyFn()}
   </>
 ));
+
+const viewports = {
+  extraSmall: {
+    name: "Portrait phone (default)",
+    styles: {
+      width: "360px",
+      height: "640px",
+    },
+  },
+  small: {
+    name: "Landscape phone (sm)",
+    styles: {
+      width: "640px",
+      height: "360px",
+    },
+  },
+  medium: {
+    name: "Tablet (md)",
+    styles: {
+      width: "768px",
+      height: "1024px",
+    },
+  },
+  large: {
+    name: "Desktop (lg)",
+    styles: {
+      width: "1024px",
+      height: "1396px",
+    },
+  },
+  extraLarge: {
+    name: "Large Desktop (xl)",
+    styles: {
+      width: "1280px",
+      height: "800px",
+    },
+  },
+};
+
+addParameters({
+  viewport: {
+    viewports,
+  },
+});
