c2614d50de9cb98a45b731f5f575154d2df16940
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-03 19:26:39 -0300

    feat: ensure media values constants break points

--- src/components/Style.js
+++ src/components/Style.js
@@ -1,5 +1,14 @@
 import styled, { css } from "styled-components";
 
+export const BreadkpointsSize = {
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+
+export const breakAt = (size) => `@media (min-width: ${size}px)`;
+
 export const Root = styled.div`
   color: #fff;
   padding: 100px 0;
@@ -18,15 +27,15 @@ export const Title = styled.h1`
 `;
 
 export const Container = styled.div`
-  background-color: red;
   width: 100%;
-  padding: 0 8px;
+  padding: 0 16px;
 
-  @media (min-width: 768px) {
-    padding: 0 16px;
+  ${breakAt(BreadkpointsSize.sm)} {
+    padding: 0 48px;
   }
 
-  @media (min-width: 1200px) {
+  ${breakAt(BreadkpointsSize.xl)} {
+    padding: 0;
     width: 1140px;
     margin: 0 auto;
   }
