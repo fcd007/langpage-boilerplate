d546cc06a71165575ff0a5fe1f224ba36a1d3356
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-08 19:33:28 -0300

    docs: change storie Hero

--- src/stories/Hero.stories.js
+++ src/stories/Hero.stories.js
@@ -3,6 +3,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import headerCovidImage from "../assets/images/covid-isolation.jpg";
 import headerWomanImage from "../assets/images/mask-woman.jpg";
+import Heading from "../components/Heading/Heading";
 
 export default {
   title: "Hero",
@@ -10,19 +11,20 @@ export default {
 };
 
 export const usage = () => (
-  <Hero
-    title="Plataforma de Gestão de UBS e Áreas Hospitalares"
-    image={headerCovidImage}
-  >
+  <Hero image={headerCovidImage}>
+    <h1>Plataforma de Gestão de UBS e Áreas Hospitalares</h1>
     <p>O Smartye provê gestão em tempo real no ambiente de UBSs e Hospitais</p>
   </Hero>
 );
 
 export const ListContent = () => (
-  <Hero
-    title="Plataforma de Gestão de UBS e Áreas Hospitalares"
-    image={headerWomanImage}
-  >
+  <Hero image={headerWomanImage}>
+    <Heading>
+      <h1>
+        Plataforma de <strong>Gestão Inteligente</strong> <br />
+        de Áreas Hospitalares
+      </h1>
+    </Heading>
     <ul>
       <li>Controle de aglomerações em ambientes da unidade de saúde;</li>
       <li>Monitoramento em tempo real distanciamento e fluxo de pessoas;</li>
