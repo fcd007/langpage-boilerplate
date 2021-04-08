36d549543f83a70a777e1bad1f694a19d71976d3
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-08 19:33:09 -0300

    docs: create storie Heading

---
+++ src/stories/Heading.stories.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Heading from "../components/Heading/Heading";
+
+export default {
+  title: "Heading",
+  component: Heading,
+};
+
+export const usage = () => (
+  <>
+    <Heading>
+      <h1>Título</h1>
+    </Heading>
+    <Heading>
+      <h2>Título</h2>
+    </Heading>
+    <Heading>
+      <h3>Título</h3>
+    </Heading>
+    <Heading>
+      <h4>Título</h4>
+    </Heading>
+    <Heading>
+      <h5>Título</h5>
+    </Heading>
+    <Heading>
+      <h6>Título</h6>
+    </Heading>
+  </>
+);
