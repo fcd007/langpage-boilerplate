5a047f06001f0a3048678fe104c834c149f5d36c
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-17 22:39:58 -0300

    feat: add title introduction stories

---
+++ src/stories/0-Welcome.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default {
+  title: "Introduction/Welcome",
+};
+
+export const toLandPageBoilerplate = () => (
+  <div>
+    <h1>Landpage Boilerplate</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat deleniti
+      soluta praesentium possimus! Aliquid, quibusdam dicta optio quidem porro
+      iure! Dolores illo tenetur asperiores exercitationem, beatae tempore
+      consectetur cumque eveniet.
+    </p>
+  </div>
+);
