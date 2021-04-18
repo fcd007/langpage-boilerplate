1f2d4ca4739920806ac9d508094bb90cde713532
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-18 20:34:15 -0300

    feat: ensure add stories global styles

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
     "../src/stories/**/*.stories.@(js|jsx|ts|tsx|mdx)",
     "../src/components/**/*.stories.@(js|jsx|ts|tsx|mdx)",
     "../src/styles/**/*.stories.@(js|mdx)",
+    "../src/styles/*.stories.(js|mdx)",
   ],
   addons: [
     "@storybook/addon-links",
