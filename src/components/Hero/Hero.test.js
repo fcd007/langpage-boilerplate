0744fc2c561516c89895f55fd23aaf23113fed0f
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-19 08:54:00 -0300

    test: ensure with test image resource background

--- src/components/Hero/Hero.test.js
+++ src/components/Hero/Hero.test.js
@@ -1,17 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
 
 import Hero from "./Hero";
 
 test("should Hero with children", () => {
   const component = render(
-    <Hero>
-      <p>Claudeilton Dantas</p>
-    </Hero>
+    <>
+      <Hero>
+        <p>Claudeilton Dantas</p>
+      </Hero>
+    </>
   );
 
-  component.debug(); //exibindo o DOM
-
   expect(component.getByText("Claudeilton Dantas")).toBeInTheDocument();
+});
+
+test("Renders image background", () => {
+  const image = "http://test/image.jpg";
+  const { getByTestId } = render(<Hero image={image} />);
+
+  expect(getByTestId("hero")).toHaveStyleRule({
+    background: image,
+  });
 });
