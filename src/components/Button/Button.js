9ecdffc69b924f55296642063c68dee2c825df42
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-20 16:52:56 -0300

    change buttons colors

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,18 +2,22 @@ import React from "react";
 import propTypes from "prop-types";
 import { ButtonRoot } from "./styles";
 
-const Button = ({ type, children }) => (
-  <ButtonRoot type={type}>{children}</ButtonRoot>
+const Button = ({ type, children, color }) => (
+  <ButtonRoot type={type} color={color}>
+    {children}
+  </ButtonRoot>
 );
 
 Button.defaultProps = {
   type: "button",
   children: undefined,
+  color: "default",
 };
 
 Button.propTypes = {
   type: propTypes.string,
   children: propTypes.node,
+  color: propTypes.oneOf(Object.values(ButtonColors)),
 };
 
 export default Button;
