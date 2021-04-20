cde15123e9c3fb36a3960c71712144c73faea123
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-20 16:53:08 -0300

    docs: change buttons colors

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,9 +1,18 @@
 import React from "react";
 import Button from "./Button";
+import { Toolbar } from "./styles";
 
 export default {
   title: "Components/Button",
   component: Button,
 };
 
-export const usage = () => <Button>Meu Botton</Button>;
+export const usage = () => (
+  <div>
+    <Toolbar>
+      <Button color="default">Default</Button>
+      <Button color="primary">Primary</Button>
+      <Button color="error">Error</Button>
+    </Toolbar>
+  </div>
+);
