a334499a33e1a71c758d04b6fad676c858960bd2
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-20 16:53:24 -0300

    feat: change buttons colors

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,13 +3,36 @@ import { theme } from "styles/ThemeProvider";
 
 const { colors } = theme;
 
+export const ButtonColors = {
+  default: "default",
+  primary: "primary",
+  error: "error",
+};
+
+const getColorButton = ({ color }) => {
+  switch (color) {
+    case ButtonColors.primary:
+      return theme.colors.primary.main;
+    case ButtonColors.error:
+      return theme.colors.error.main;
+    default:
+      return "#BDBDBD";
+  }
+};
+
 export const ButtonRoot = styled.button`
   font-size: 1rem;
   font-weight: 400;
   text-transform: uppercase;
   padding: 12px 36px;
   cursor: pointer;
-  background-color: ${colors.primary.main};
-  border: 2px solid ${colors.primary.main};
-  color: ${colors.text.main};
+  background-color: ${getColorButton};
+  border: 2px solid ${getColorButton};
+  color: ${colors.text.light};
+`;
+
+export const Toolbar = styled.div`
+  & > * {
+    margin-right: 8px;
+  }
 `;
