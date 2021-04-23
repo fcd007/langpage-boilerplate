33eaa155c100ab5ed3f75cef4a7ce426fad00bf5
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-23 16:54:07 -0300

    feat: ensure icons grip feature

--- src/components/atoms/Features/Feature.stories.js
+++ src/components/atoms/Features/Feature.stories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLaptop } from "react-icons/fa";
+import { FaLaptop, FaKey, FaParking, FaUser } from "react-icons/fa";
 import Feature from "./Feature";
 
 export default {
@@ -11,4 +11,21 @@ export const usage = () => (
   <Feature icon={<FaLaptop />} title="Teste">
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
   </Feature>
+);
+
+export const withGrid = () => (
+  <div>
+    <Feature icon={<FaUser />} title="Controle espaços">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    </Feature>
+    <Feature icon={<FaParking />} title="Plataforma RT">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    </Feature>
+    <Feature icon={<FaLaptop />} title="Gestão Inteligente">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    </Feature>
+    <Feature icon={<FaKey />} title="Acessibilidade">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    </Feature>
+  </div>
 );
