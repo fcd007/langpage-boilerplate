c94977fdecd06d02cd0dd868d1ebc05e526255b7
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-23 12:59:22 -0300

    feat: add feature component

---
+++ src/components/atoms/Features/Feature.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Feature from "./Feature";
+
+export default {
+  title: "Components/Atoms/Feature",
+  component: "Feature",
+};
+
+export const usage = () => <Feature title="Teste">Mais testes</Feature>;
