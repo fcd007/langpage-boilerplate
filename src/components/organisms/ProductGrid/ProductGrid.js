f6cc26d89a72a0ac73b7d7f58a5215a5f9d46044
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-30 17:32:35 -0300

    feat: change product grid element to renders

--- src/components/organisms/ProductGrid/ProductGrid.js
+++ src/components/organisms/ProductGrid/ProductGrid.js
@@ -1,36 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import { Root } from "./style";
+import { Toolbar } from "./style";
 import { GridRoot } from "components/atoms/Grid/style";
+
 import Card from "components/molecules/Card/Card";
 import CardBody from "components/molecules/CardBody/CardBody";
 import CardMedia from "components/molecules/CardMedia/CardMedia";
 import Heading from "components/atoms/Heading/Heading";
-import ButtonWrapper from "components/atoms/Button/Button";
+import Button from "components/atoms/Button/Button";
 
-const ProductGrid = ({ products }) => (
-  <Root>
-    <GridRoot md={3}>
-      {products.map((product) => (
-        <Card key={product.id}>
-          <CardMedia image={product.image} />
-          <CardBody>
-            <Heading>
-              <h6>{product.title}</h6>
-            </Heading>
-            <p>{product.summary}</p>
-            <div>
-              <ButtonWrapper variant="link" color="primary">
-                SAIBA MAIS
-              </ButtonWrapper>
-            </div>
-          </CardBody>
-        </Card>
-      ))}
-    </GridRoot>
-  </Root>
-);
+const ProductGrid = ({ products }) => {
+  const [showAll, setShowAll] = useState(false);
+  const filteredProducts = showAll ? products : products.slice(0, 3);
+
+  return (
+    <>
+      <GridRoot md={3}>
+        {filteredProducts.map((product) => (
+          <Card key={product.id}>
+            <CardMedia image={product.image} />
+            <CardBody>
+              <Heading>
+                <h6>{product.title}</h6>
+              </Heading>
+              <p>{product.summary}</p>
+              <Toolbar>
+                <Button variant="link" color="primary">
+                  SAIBA MAIS
+                </Button>
+              </Toolbar>
+            </CardBody>
+          </Card>
+        ))}
+      </GridRoot>
+      {!showAll && (
+        <Toolbar>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setShowAll(true);
+            }}
+          >
+            Lista Completa de Serviços
+          </Button>
+        </Toolbar>
+      )}
+    </>
+  );
+};
 ProductGrid.defaultProps = {
   products: [],
 };
