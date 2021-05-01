cc9d6733eb2178021a6aa4e79d08987159028dc2
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-30 22:17:56 -0300

    test: change test multiple elements

--- src/components/organisms/ProductGrid/ProductGrid.test.js
+++ src/components/organisms/ProductGrid/ProductGrid.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "test-utils";
+import { render, screen, fireEvent } from "test-utils";
 
 import ProductGrid from "./ProductGrid";
 
@@ -12,8 +12,25 @@ const buildProducts = (size) => {
 
   return result;
 };
-test("With 6 products show only 3 items", () => {
-  render(<ProductGrid products={buildProducts(6)} />);
 
-  expect(screen.getAllByRole("heading").length).toBe(3);
+describe.each([
+  [1, 1],
+  [2, 2],
+  [3, 3],
+  [4, 3],
+  [6, 3],
+])("With %i products", (size, shoowOnly) => {
+  test(`Show only ${shoowOnly} items`, () => {
+    render(<ProductGrid products={buildProducts(size)} />);
+
+    expect(screen.getAllByRole("heading").length).toBe(shoowOnly);
+  });
+
+  test("Show all the products when button is clicked", async () => {
+    render(<ProductGrid products={buildProducts(size)} />);
+
+    await fireEvent.click(screen.getByText("Lista Completa de Serviços"));
+
+    expect(screen.getAllByRole("heading").length).toBe(size);
+  });
 });
