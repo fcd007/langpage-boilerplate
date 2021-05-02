c236ac3ce61e46c487ac109c2095f85b2d3d4cbb
Claudeilton Dantas <fcd007@hotmail.com>
2021-05-02 11:28:59 -0300

    docs: add document Accordion storie

---
+++ src/components/atoms/Accordion/Accordion.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Accordion from "./Accordion";
+
+export default {
+  title: "Components/Atoms/Accordion",
+  component: Accordion,
+};
+
+export const usage = () => (
+  <Accordion title="Como solicito acesso a plataforma SmartEye?">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
+    voluptatibus enim et quaerat minima blanditiis.
+  </Accordion>
+);
