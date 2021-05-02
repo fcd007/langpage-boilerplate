2c18317944019fd3d3778ce075d7134a8396b219
Claudeilton Dantas <fcd007@hotmail.com>
2021-05-01 21:54:29 -0300

    chore: add atribute css overflow hidden

--- src/components/molecules/Card/style.js
+++ src/components/molecules/Card/style.js
@@ -4,6 +4,7 @@ import { allTheme as theme } from "styles/ThemeProvider";
 export const Root = styled.div`
   width: 350px;
   border-radius: 4px;
+  overflow: hidden;
   border-color: ${theme.light.colors.primary.light};
   background-color: ${theme.light.colors.background.main};
 `;
--- src/components/molecules/CardMedia/style.js
+++ src/components/molecules/CardMedia/style.js
@@ -4,7 +4,6 @@ export const StyledMedia = styled.div`
   background-image: url(${(props) => props.image});
   background-position: center center;
   background-size: cover;
-  border-radius: 4px 4px 0 0;
   width: 350px;
   height: 250px;
 `;
