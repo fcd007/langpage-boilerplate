c91d90d0f7406a7c87550dfea719537314c66d22
Claudeilton Dantas <fcd007@hotmail.com>
2021-05-02 21:12:58 -0300

    docs: update core document

--- src/components/atoms/Accordion/Accordion.stories.js
+++ src/components/atoms/Accordion/Accordion.stories.js
@@ -8,7 +8,7 @@ export default {
 };
 
 export const usage = () => (
-  <Accordion title="Como solicito acesso a plataforma SmartEye?">
+  <Accordion title="Como funciona o projeto SmartEye?">
     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
     voluptatibus enim et quaerat minima blanditiis.
   </Accordion>
