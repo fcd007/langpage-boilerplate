2cad1bd776ef250aaf05639c4284f53faee3fefc
Claudeilton Dantas <fcd007@hotmail.com>
2021-05-04 21:11:29 -0300

    docs: document accordion use

--- src/components/atoms/Accordion/Accordion.stories.js
+++ src/components/atoms/Accordion/Accordion.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Accordion from "./Accordion";
+import AccordionGroup from "./AccordionGroup";
 
 export default {
   title: "Components/Atoms/Accordion",
@@ -12,4 +13,23 @@ export const usage = () => (
     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
     voluptatibus enim et quaerat minima blanditiis.
   </Accordion>
+);
+
+export const withGroup = () => (
+  <AccordionGroup>
+    <Accordion title="Como funciona o projeto SmartEye?">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
+      voluptatibus enim et quaerat minima blanditiis.
+    </Accordion>
+
+    <Accordion title="Como recuperar senha ou acesso bloqueado?">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
+      voluptatibus enim et quaerat minima blanditiis.
+    </Accordion>
+
+    <Accordion title="Como solicito acesso a plataforma SmartEye?">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
+      voluptatibus enim et quaerat minima blanditiis.
+    </Accordion>
+  </AccordionGroup>
 );
