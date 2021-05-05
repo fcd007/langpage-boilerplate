4b98d45b38c655424f3c13bb413615bc98ce5473
Claudeilton Dantas <fcd007@hotmail.com>
2021-05-04 21:11:40 -0300

    feat: add component change

---
+++ src/components/atoms/Accordion/AccordionGroup.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const AccordionGroup = ({ children }) => {
+  const [index, setIndex] = useState(-1);
+
+  return React.Children.map(children, (child, childIndex) =>
+    React.cloneElement(child, {
+      open: childIndex === index,
+      onChange: () => {
+        setIndex(childIndex);
+      },
+    })
+  );
+};
+
+AccordionGroup.defaultProps = {
+  children: undefined,
+};
+
+AccordionGroup.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AccordionGroup;
