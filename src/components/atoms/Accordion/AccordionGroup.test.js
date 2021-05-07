f38e70a94bd6e709f8ceff219d33065213724ed3
Claudeilton Dantas <fcd007@hotmail.com>
2021-05-06 21:26:03 -0300

    test: add test on click render title and text

--- src/components/atoms/Accordion/AccordionGroup.test.js
+++ src/components/atoms/Accordion/AccordionGroup.test.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { render, screen } from "test-utils";
+import { render, screen, fireEvent } from "test-utils";
 import Accordion from "./Accordion";
 import AccordionGroup from "./AccordionGroup";
 
+const title = "My title";
+const renderGroup = (numberChilders) => {
+  const accordions = [];
+
+  for (let index = 0; index < numberChilders; index++) {
+    accordions.push(
+      <Accordion key={index} title={`Title ${index}`}>
+        Child {index}
+      </Accordion>
+    );
+  }
+
+  return render(<AccordionGroup>{accordions}</AccordionGroup>);
+};
+
 test("Renders with one child", () => {
-  const title = "My title";
   render(
     <AccordionGroup>
       <Accordion title={title} />
@@ -15,7 +29,6 @@ test("Renders with one child", () => {
 });
 
 test("Renders with 3 child", () => {
-  const title = "My title";
   render(
     <AccordionGroup>
       <Accordion title={title} />
@@ -25,4 +38,37 @@ test("Renders with 3 child", () => {
   );
 
   expect(screen.getAllByText(title)).toHaveLength(3);
+});
+
+test("renders with all accordions closed ", () => {
+  renderGroup(3);
+  expect(screen.queryByText("Child 0")).not.toBeInTheDocument();
+  expect(screen.queryByText("Child 1")).not.toBeInTheDocument();
+  expect(screen.queryByText("Child 2")).not.toBeInTheDocument();
+});
+
+test("opens accordions with clicked ", async () => {
+  renderGroup(3);
+
+  await fireEvent.click(screen.getByText("Title 0"));
+
+  expect(screen.queryByText("Child 0")).toBeInTheDocument();
+  expect(screen.queryByText("Child 1")).not.toBeInTheDocument();
+  expect(screen.queryByText("Child 2")).not.toBeInTheDocument();
+});
+
+test("closes accordions with clicked ", async () => {
+  renderGroup(3);
+
+  await fireEvent.click(screen.getByText("Title 1"));
+
+  expect(screen.queryByText("Child 0")).not.toBeInTheDocument();
+  expect(screen.queryByText("Child 1")).toBeInTheDocument();
+  expect(screen.queryByText("Child 2")).not.toBeInTheDocument();
+
+  await fireEvent.click(screen.getByText("Title 1"));
+
+  expect(screen.queryByText("Child 0")).not.toBeInTheDocument();
+  expect(screen.queryByText("Child 1")).not.toBeInTheDocument();
+  expect(screen.queryByText("Child 2")).not.toBeInTheDocument();
 });
